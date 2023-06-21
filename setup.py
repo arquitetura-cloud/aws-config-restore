8032517afdf96e471c777e518245ea7026706fd5
alexandre <alexandre.gomes@operacaomulticloud.com>
2023-06-21 10:47:20 -0300

    Project Converted to PyPI package.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='aws-config-restore',
-    version='0.1',
+    version='0.1.0',
     packages=['app'],
     url='https://github.com/arquitetura-cloud/aws-config-restore',
-    license='Apache 2.0',
+    license='MIT',
     author='CCoE Embratel',
     author_email='arquitetura-cloud@operacaomulticloud.com',
     description='Tool to restore configurations from AWS Config'
